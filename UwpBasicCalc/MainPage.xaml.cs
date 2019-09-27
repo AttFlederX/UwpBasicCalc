fc53949991b86c12b5d7b237fc372b3b8aae0d0b
Dmitry Afanasiev <dmitry.afanasiev@cigen.me>
2019-09-27 23:26:52 +0300

    Add base arithmetic logic

--- UwpBasicCalc/MainPage.xaml.cs
+++ UwpBasicCalc/MainPage.xaml.cs
@@ -22,8 +22,9 @@ namespace UwpBasicCalc
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private bool _isOnStandby;
+        private bool _isOnStandby; // determines whether the next digit entered should replace the previous content
         private string _currentOperation;
+        private double _accumulator;
 
         public MainPage()
         {
@@ -37,20 +38,132 @@ namespace UwpBasicCalc
         {
             if (sender is Button digitButton)
             {
-                var digit = digitButton.Content.ToString();
-                if (_isOnStandby)
+                if (outputTextBlock.Text != "0")
                 {
-                    outputTextBlock.Text = digit;
-                    _isOnStandby = false;
-                }
-                else
-                {
-                    outputTextBlock.Text += digit;
+                    var digit = digitButton.Content.ToString();
+                    if (_isOnStandby)
+                    {
+                        outputTextBlock.Text = digit;
+                        _isOnStandby = false;
+                    }
+                    else
+                    {
+                        outputTextBlock.Text += digit;
+                    }
                 }
             }
         }
 
-        private void ZeroButton_Click(object sender, RoutedEventArgs e)
+        private void DecimalPointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isOnStandby && !outputTextBlock.Text.Contains('.'))
+            {
+                outputTextBlock.Text += ".";
+            }
+        }
+
+        private void ChangeSignButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isOnStandby && outputTextBlock.Text != "0" && outputTextBlock.Text != "0.")
+            {
+                if (outputTextBlock.Text.First() != '-')
+                {
+                    outputTextBlock.Text = "-" + outputTextBlock.Text;
+                }
+                else
+                {
+                    outputTextBlock.Text = outputTextBlock.Text.Substring(1); // remove leading minus
+                }
+            }
+        }
+
+        private void ClearEntryButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isOnStandby)
+            {
+                outputTextBlock.Text = "0.";
+                _isOnStandby = true;
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBlock.Text = "0.";
+
+            _isOnStandby = true;
+            _currentOperation = string.Empty;
+            _accumulator = 0;
+        }
+
+        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isOnStandby)
+            {
+                var len = outputTextBlock.Text.Length;
+                if (len > 1)
+                {
+                    outputTextBlock.Text = outputTextBlock.Text.Substring(0, len - 1);
+                }
+                else
+                {
+                    outputTextBlock.Text = "0.";
+                    _isOnStandby = true;
+                }
+            }
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button operatorButton)
+            {
+                if (!_isOnStandby && string.IsNullOrWhiteSpace(_currentOperation))
+                {
+                    _accumulator = double.Parse(outputTextBlock.Text);
+                    _currentOperation = operatorButton.Content.ToString();
+                    _isOnStandby = true;
+                }
+            }
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_isOnStandby && !string.IsNullOrWhiteSpace(_currentOperation))
+            {
+                var operand = double.Parse(outputTextBlock.Text);
+                double? result = 0;
+
+                switch (_currentOperation)
+                {
+                    case "+":
+                        result = _accumulator + operand;
+                        break;
+                    case "-":
+                        result = _accumulator - operand;
+                        break;
+                    case "*":
+                        result = _accumulator * operand;
+                        break;
+                    case "/":
+                        if (operand == 0) { result = null; }
+                        else
+                        {
+                            result = _accumulator / operand;
+                        }
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Invalid operation received in EqualsButton_Click");
+                }
+
+                outputTextBlock.Text = result?.ToString() ?? "Error";
+
+                _isOnStandby = true;
+                _accumulator = 0;
+                _currentOperation = string.Empty;
+            }
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
         {
 
         }
